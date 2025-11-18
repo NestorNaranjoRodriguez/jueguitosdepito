0296f3f3db1fcf438d6f9fb17da1cd628966a018
AntonioMeliannHerrera <antoniomiguelmelianherrera@alumno.ieselrincon.es>
2025-11-18 16:44:06 +0000

    dadosv2

--- dados.py
+++ dados.py
@@ -1,9 +1,8 @@
 import random
 
 def juego_pares_y_nones():
-    print("\n🎲 JUEGO: PARES Y NONES 🎲")
+    print("\nJUEGO: PARES Y NONES")
 
-    # Pedir número de dados
     dados = int(input("¿Cuántos dados quieres lanzar?: "))
 
     puntos_j1 = 0
@@ -11,26 +10,28 @@ def juego_pares_y_nones():
 
     print("\nLanzando dados...\n")
 
-    for i in range(dados):
+    contador = 1
+    while contador <= dados:
         tiro = random.randint(1, 6)
-        print(f"Dado {i+1}: {tiro}")
+        print("Dado " + str(contador) + ": " + str(tiro))
 
         if tiro % 2 == 0:
             puntos_j1 += 1
         else:
             puntos_j2 += 1
 
-    print("\n--- RESULTADOS ---")
-    print(f"Jugador 1 (pares): {puntos_j1} puntos")
-    print(f"Jugador 2 (impares): {puntos_j2} puntos")
+        contador += 1
 
-    # Determinar ganador
+    print("\n--- RESULTADOS ---")
+    print("Jugador 1 (pares): " + str(puntos_j1) + " puntos")
+    print("Jugador 2 (impares): " + str(puntos_j2) + " puntos")
+
     if puntos_j1 > puntos_j2:
-        print("🏆 ¡Gana el Jugador 1!")
+        print("Gana el Jugador 1")
     elif puntos_j2 > puntos_j1:
-        print("🏆 ¡Gana el Jugador 2!")
+        print("Gana el Jugador 2")
     else:
-        print("🤝 ¡Empate!")
+        print("Empate")
 
 if __name__ == "__main__":
     juego_pares_y_nones()
